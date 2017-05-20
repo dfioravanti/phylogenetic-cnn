8a1ade736dae41687375508aba5eb8d5c7a77b05
Valerio Maggio <vmaggio@fbk.eu>
2017-05-20 19:47:16 +0200

    Revised and better organised settings (see below for details)
    Settings now have been properly documented and include all settings
    necessary to compile and fit Keras models.
    However, support for `fit_generator` is still missing and will be added soon.
    
    The most important change in settings is that now processing methods
    (e.g. "feature scaling/ranking", "losses", "optimizers") can
    now be specified either as strings or as Python (function/class) objects.
    This allows to make settings even more flexible and exploits all
    the benefits of having a settings file as a Python importable module!

--- phylogenetic/data_analysis_plan/settings.py
+++ phylogenetic/data_analysis_plan/settings.py
@@ -1,87 +1,176 @@
 # ============================================
-# -- DAP Section
+# A. DAP Section
 # ============================================
 
-# -- Feature Scaling Method
+# ---------------------------
+# 0. Cross Validation Section
+# ---------------------------
 
-# Control whether to apply Feature Scaling
-# Default: True
-apply_feature_scaling = True
-
-# Choices are: std, minmax0, norm_l2, minmax
-# -- Feature Scaling Choices
-STD = 'std'
-MINMAX0 = 'minmax0'
-MINMAX = 'minmax'
-NORM_L2 = 'norm_l2'
-
-feature_scaling_method = STD
-
-# -- Feature Ranking Method
-
-# Choices are: ReliefF, random, KBest
-RELIEFF = 'ReliefF'
-RANDOM = 'rnd'
-KBEST = 'KBest'
-feature_ranking_method = RELIEFF
-
-# Only used with ReliefF
-relief_k = 3  # K parameter for the (internal) KNN
-
-# Only used with KBest
-kbest = 10
-
-# Percentage of total features to consider in the ranking list
-feature_selection_percentage = [5, 25, 50, 75, 100]
-# Decide whether to include the first Top feature resulting from ranking
-include_top_feature = False
-
-# random labels
-use_random_labels = False  # Set to True to randomly shuffle training set labels
-
-# Categorical Labels: Decide whether to one-hot-encode labels
-to_categorical = True
-
-# -- Cross Validation
 # No. Repetitions
 Cv_N = 1
 # No. Fold
 Cv_K = 2
 
-# Whether to apply Stratification
+# Apply Stratification to labels when
+# generating folds.
 stratified = True
 
+# Enable/Disable Use of Random Labels
+# If True, a random shuffling to training set labels
+# will be applied.
+use_random_labels = False
+
+# Categorical Labels:
+# Decide whether to apply one-hot-encode to labels
+to_categorical = True
+
+# --------------------------
+# 1. Feature Scaling Section
+# --------------------------
+
+# Enable/Disable Feature Scaling
+apply_feature_scaling = True
+
+# Feature Scaling method
+# ----------------------
+# This can either be a string or
+# an actual sklearn Transformer object
+# (see sklearn.preprocessing)
+from .scaling import StandardScaler
+feature_scaler = StandardScaler(copy=False)
+
+# --------------------------
+# 2. Feature Ranking Section
+# --------------------------
+
+# This can be eitehr a string or a
+# (custom) function object,
+from .ranking import kbest_ranking
+feature_ranker = kbest_ranking
+
+# -----------------
+# 2.1 Feature Steps
+# _________________
+
+# Ranges (expressed as percentage wrt. the total)
+# of features to consider when generating feature steps
+
+# Default: 5%, 25%, 50%, 75%, 100% (all)
+feature_ranges = [5, 25, 50, 75, 100]
+
+# Include top feature in the feature steps
+use_top_feature = False
+
 # =================================================
-# -- Machine Learning Models Hyperparameter Section
+# B. Machine Learning Models HyperParameter Section
 # =================================================
 
-# -- Optimizers Choices
-ADAM = 'adam'
-SGD = 'sgd'
-RMSPROP = 'rmsprop'
+# Use of scikit Pipelines!!
 
-# -- Optimisers Specific Settings
+# =================================================
+# C. Deep Learning Models Hyperparameter Section
+# =================================================
 
-# == SGD ==
-nesterov = True
-sgd_lr = 0.001
-momentum = 0.9
-decay = 1e-06
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# This following section contains settings
+# that will be used only by the
+# `DeepLearningDAP` class!
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-==-=-=
 
-# == RMSPROP ==
-rmsprop_epsilon = 1e-08
-rmsprop_lr = 0.001
-rho = 0.9
+# -------------------
+# 1 Model Fit Section
+# ___________________
 
-# == ADAM ==
-adam_epsilon = 1e-08
-adam_lr = 0.001
-beta_1 = 0.9
-beta_2 = 0.999
-
-# Fit Settings
+# No. of Epochs
 epochs = 2
+
+# Size of the Batch
 batch_size = 32
-optimizer = 'adam'  # Choices are: sgd, adam, rmsprop
+
+# Verbosity level of `fit` method
 fit_verbose = 2
 
+# Validation split
+# ----------------
+# (Automatically ignored if `validation_data` is provided)
+validation_split = 0.0  # Default: no split
+
+# Shuffle (boolean)
+# -----------------
+# Whether to shuffle the samples at each epoch.
+shuffle = True  # Shuffle samples at each epoch, by default.
+
+# Class Weights
+# -------------
+# Dictionary mapping classes to a weight value
+# used for scaling the loss function (during training only).
+class_weight = None
+
+# Sample Weights
+# --------------
+# Numpy array of weights for the training samples,
+# used for scaling the loss function (during training only).
+# You can either pass a flat (1D) Numpy array with the same
+# length as the input samples
+# (1:1 mapping between weights and samples),
+# or in the case of temporal data, you can pass a 2D array
+# with shape (samples, sequence_length), to
+# apply a different weight to every timestep of every sample.
+# In this case you should make sure to specify
+# sample_weight_mode="temporal" in compile().
+sample_weight = None
+
+# Initial Epoch
+# -------------
+# Epoch at which to start training
+# (useful for resuming a previous training run).
+initial_epoch = 0  # Default: 0 -  first epoch!
+
+# Additional Callbacks
+# --------------------
+# By default, the `keras.callbacks.ModelSelection` callback
+# will be applied at each fit, in addition to the default
+# `keras.callbacks.History`.
+#
+# To automatically plug additional callbacks into
+# model fit, please add configured keras Callbacks objects
+# in the list below
+callbacks = []
+
+# -----------------------
+# 2 Model Compile Section
+# _______________________
+
+# Loss Function
+# --------------
+# This may be either a string or a function object
+# (see keras.losses for examples)
+loss='categorical_crossentropy'
+
+# Loss Weights
+# -------------
+# List of weights to associate to losses
+# (in case of multi-output networks)
+loss_weights = None
+
+# Additional Compile Settings
+# ---------------------------
+# Additional Compile settings directly
+# passed to Theano functions. Ignored by Tensorflow
+extra_compilation_parameters = {}
+
+# Loss Metric(s)
+# --------------
+# List of metrics to optimise in the training.
+# These can either be strings or function objects
+# Default metric is accuracy.
+# (see keras.metrics for examples)
+metrics = ['accuracy']
+
+# Optimizer
+# ---------
+# This can either be a string or an
+# optimizer object (see keras.optimizers)
+from keras.optimizers import Adam
+optimizer = Adam(lr=0.001, decay=1e-06,
+                 epsilon=1e-08, beta_1=0.9, beta_2 = 0.999)
