258f0ce62c13c7f182b9034a5621aaf8301ed2f0
Diego Fioravanti <fioravanti.diego@gmail.com>
2017-05-23 11:38:07 +0200

    Refactored the script that generates synthetics data for easier usability

--- phylogenetic/generate_synthetic_data.R
+++ phylogenetic/generate_synthetic_data.R
@@ -1,78 +1,114 @@
-# This script generates synthetic data  starting from the HS and CDflare datasets  
+# This script generates synthetic data  starting from the real datasets
 
 library(compositions)
 library(mvtnorm)
 set.seed(1234)
 
-coordinates_cdf <- read.delim("datasets/coordinates/coordinates_cdf.txt")
-var_names <- colnames(coordinates_cdf)
+# Total number of sample to be generated
+nb_total_output_samples <- 1000
 
-table_tr <- read.delim("datasets/true_data/HS_CDf/Sokol_16S_taxa_HS_CDflare_commsamp_training.txt")
-table_ts <- read.delim("datasets/true_data/HS_CDf/Sokol_16S_taxa_HS_CDflare_commsamp_validation.txt")
+# Proportion of test data to be generated
+proportion_test = 0.20
+
+# Path configuration
+
+desease <- "iCDr"
+coordinates_path <- paste("datasets/coordinates/coordinates_", tolower(desease), ".txt", sep="")
+training_samples_path <- paste("datasets/true_data/HS_", desease, "/Sokol_16S_taxa_HS_",
+                                desease, "_commsamp_training.txt", sep="")
+test_samples_path <- paste("datasets/true_data/HS_", desease, "/Sokol_16S_taxa_HS_",
+                           desease, "_commsamp_validation.txt", sep="")
+training_target_path <- paste("datasets/true_data/HS_", desease, "/Sokol_16S_taxa_HS_",
+                               desease, "_commsamp_training_lab.txt", sep="")
+test_target_path <- paste("datasets/true_data/HS_", desease, "/Sokol_16S_taxa_HS_",
+                           desease, "_commsamp_validation_lab.txt", sep="")
+
+output_dir_path <- paste("datasets/synthetic_data/HS_", desease, sep="")
+dir.create(output_dir_path)
+
+training_samples_output_path <- paste(output_dir_path, "/Sokol_16S_taxa_HS_",
+                                      desease, "_commsamp_training.txt", sep="")
+training_targets_output_path <- paste(output_dir_path, "/Sokol_16S_taxa_HS_",
+                                     desease, "_commsamp_training_lab.txt", sep="")
+test_samples_output_path <- paste(output_dir_path, "/Sokol_16S_taxa_HS_",
+                                  desease, "_commsamp_test.txt", sep="")
+test_targets_output_path <- paste(output_dir_path, "/Sokol_16S_taxa_HS_",
+                                  desease, "_commsamp_test_lab.txt", sep="")
+
+
+# Compute the number of healty and sick sample for both training and test
+training_target <- read.delim(training_target_path, header = FALSE)
+nb_helthy_training = sum(training_target == 0)
+nb_sample_training = dim(training_target)[1]
+
+test_target <- read.delim(test_target_path, header = FALSE)
+nb_helthy_test = sum(test_target == 0)
+nb_sample_test = dim(test_target)[1]
+
+# Load samples
+features_names <- colnames(read.delim(coordinates_path))
+training_samples <- read.delim(training_samples_path)
+test_samples <- read.delim(test_samples_path)
 
 #HS
-hs_tr <-read.delim("datasets/true_data/HS_CDf/Sokol_16S_taxa_HS_CDflare_commsamp_training.txt")[1:27,-1]
-hs_ts <-read.delim("datasets/true_data/HS_CDf/Sokol_16S_taxa_HS_CDflare_commsamp_validation.txt")[1:11,-1]
-hs <- rbind(hs_tr,hs_ts)
+healty_training <- training_samples[1:nb_helthy_training, -1]
+healty_test <- test_samples[1:nb_helthy_test, -1]
+original_healty <- rbind(healty_training, healty_test)
 
-# View data as compositional data
-hs_cmp <- rcomp(hs)
-#Apply log-ratio trasformation 
-transf_hs <- ilr(hs_cmp) 
+# Apply log-ratio trasformation, since ilr need the data as compositional data
+# we apply the rcomp before we call ilr
+original_healty_ilr <- ilr(rcomp(original_healty))
 
 #Compute mean and variance
-vect_mean_hs <- mean(transf_hs)
-cov_matrix_hs <- cov(transf_hs)
+means_healty <- mean(original_healty_ilr)
+covariance_healty <- cov(original_healty_ilr)
 
 #CDf
-cdf_tr <-read.delim("datasets/true_data/HS_CDf/Sokol_16S_taxa_HS_CDflare_commsamp_training.txt")[28:dim(table_tr)[1],-1]
-cdf_ts <-read.delim("datasets/true_data/HS_CDf/Sokol_16S_taxa_HS_CDflare_commsamp_validation.txt")[12:dim(table_ts)[1],-1]
-cdf <- rbind(cdf_tr, cdf_ts)
+sick_training <- training_samples[(nb_helthy_training + 1):nb_sample_training, -1]
+sick_test <- test_samples[(nb_helthy_test + 1):nb_sample_test, -1]
+original_sick <- rbind(sick_training, sick_test)
 
-# View data as compositional data
-cdf_cmp <- rcomp(cdf) 
-#Apply log-ratio trasformation 
-transf_cdf <- ilr(cdf_cmp)
+# Apply log-ratio trasformation, since ilr need the data as compositional data
+# we apply the rcomp before we call ilr
+original_sick_ilr <- ilr(rcomp(original_sick))
 
 #Compute mean and variance
-vect_mean_cdf <- mean(transf_cdf)
-cov_matrix_cdf <- cov(transf_cdf)
+means_sick <- mean(original_sick_ilr)
+covariance_sick <- cov(original_sick_ilr)
 
 # compute means difference
-AB <- vect_mean_cdf-vect_mean_hs
+AB <- means_sick-means_healty
 
 #compute projections
-AC <- transf_hs-vect_mean_hs
+AC <- original_healty_ilr - means_healty
 project_hs <- AC%*%AB
-AD <- transf_cdf-vect_mean_hs
+AD <- original_sick_ilr - means_healty
 project_cdf <- AD%*%AB
 
 #compute sigma
 m1 <- 0
-m2 <- sum((vect_mean_cdf-vect_mean_hs)^2)
-mu <- (vect_mean_hs+vect_mean_cdf)/2
-sigma1 <- sqrt((sum(project_hs^2)+sum(project_cdf-m2))/(dim(hs)[1]+dim(cdf)[1]))
+m2 <- sum((means_sick-means_healty)^2)
+mu <- (means_healty+means_sick)/2
+sigma1 <- sqrt((sum(project_hs^2)+sum(project_cdf-m2))/(dim(original_healty)[1]+dim(original_sick)[1]))
 
 alpha <- 1
-mean1 <- mu + alpha*sigma1*vect_mean_hs/norm(vect_mean_hs,'2')
-mean2 <- mu + alpha*sigma1*vect_mean_cdf/norm(vect_mean_cdf,'2')
+mean1 <- mu + alpha*sigma1*means_healty/norm(means_healty,'2')
+mean2 <- mu + alpha*sigma1*means_sick/norm(means_sick,'2')
 
-#number of samples
-nb_total <- 20000
-prp_sick <- (dim(cdf)/(dim(cdf) + dim(hs)))[1] # Mantaining the original proportion between sick and healty
-nb_sick <- trunc(nb_total * prp_sick)
-nb_healty <- nb_total - nb_sick
+# Mantaining the original proportion between sick and healty
+prp_sick <- (dim(original_sick)/(dim(original_sick) + dim(original_healty)))[1]
+nb_sick <- trunc(nb_total_output_samples * prp_sick)
+nb_healty <- nb_total_output_samples - nb_sick
 
-multivariate_healty <- rmvnorm(nb_healty, mean=mean1 , sigma=cov_matrix_hs, method="svd")
-multivariate_sick <- rmvnorm(nb_sick, mean=mean2, sigma=cov_matrix_cdf, method="svd")
+multivariate_healty <- rmvnorm(nb_healty, mean=mean1 , sigma=covariance_healty, method="svd")
+multivariate_sick <- rmvnorm(nb_sick, mean=mean2, sigma=covariance_sick, method="svd")
 
 synthetic_data_healty <- ilrInv(multivariate_healty)
 synthetic_data_sick <- ilrInv(multivariate_sick)
 
 #training and validation
-prp_test = 0.20
-nb_healty_ts = floor(prp_test * nb_healty)
-nb_sick_ts = floor(prp_test * nb_sick)
+nb_healty_ts = floor(proportion_test * nb_healty)
+nb_sick_ts = floor(proportion_test * nb_sick)
 index_healty_ts <- sample(1:nb_healty, nb_healty_ts)
 index_healty_tr <- setdiff(1:nb_healty, index_healty_ts)
 index_sick_ts <- sample(1:nb_sick, nb_sick_ts)
@@ -81,34 +117,36 @@ index_sick_tr <- setdiff(1:nb_sick, index_sick_ts)
 synthetic_training <- rbind(synthetic_data_healty[index_healty_tr,],
                             synthetic_data_sick[index_sick_tr,])
 names_tr <- 1:dim(synthetic_training)[1]
-synthetic_training <- cbind(names_tr, synthetic_training)
-colnames(synthetic_training) <- var_names
-write.table(synthetic_training,
-            file="datasets/synthetic_data/Sokol_16S_taxa_HS_CDflare_commsamp_training.txt",
+synthetic_training_samples <- cbind(names_tr, synthetic_training)
+colnames(synthetic_training_samples) <- features_names
+
+# Save synthetic training data
+write.table(synthetic_training_samples,
+            file=training_samples_output_path,
             quote = FALSE, sep="\t" ,row.names=F)
 
 training_labels <- matrix(c(rep(0, length(index_healty_tr)),
                             rep(1, length(index_sick_tr))), 
                           ncol = 1)
 write.table(training_labels,
-            file="datasets/synthetic_data/Sokol_16S_taxa_HS_CDflare_commsamp_training_lab.txt",
+            file=training_targets_output_path,
             quote = FALSE, sep="\t" ,
             row.names = FALSE, col.names = FALSE)
 
+# Save synthetic test data
 synthetic_test <- rbind(synthetic_data_healty[index_healty_ts,],
                         synthetic_data_sick[index_sick_ts,])
 names_ts <- 1:dim(synthetic_test)[1]
-synthetic_test <- cbind(names_ts, synthetic_test)
-colnames(synthetic_test) <- var_names
-write.table(synthetic_test,
-            file="datasets/synthetic_data/Sokol_16S_taxa_HS_CDflare_commsamp_test.txt",
+synthetic_test_samples <- cbind(names_ts, synthetic_test)
+colnames(synthetic_test_samples) <- features_names
+write.table(synthetic_test_samples,
+            file=test_samples_output_path,
             quote = FALSE, sep="\t" ,row.names=F)
 
 test_labels <- matrix(c(rep(0, length(index_healty_ts)),
                         rep(1, length(index_sick_ts))), 
                       ncol = 1)
 write.table(test_labels,
-            file="datasets/synthetic_data/Sokol_16S_taxa_HS_CDflare_commsamp_test_lab.txt",
+            file=test_targets_output_path,
             quote = FALSE, sep="\t" ,
             row.names = FALSE, col.names = FALSE)
-
